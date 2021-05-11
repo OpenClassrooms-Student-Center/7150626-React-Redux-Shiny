798fe7dda22b82ab2d656272177d23fac3740cc7
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-11 23:31:49 +0200

    Add Provider

--- src/index.jsx
+++ src/index.jsx
@@ -11,39 +11,43 @@ import Footer from './components/Footer'
 import Error from './components/Error'
 import GlobalStyle from './utils/style/GlobalStyle'
 import { ThemeProvider, SurveyProvider } from './utils/context'
+import { Provider } from 'react-redux'
+import store from './utils/store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <ThemeProvider>
-        <SurveyProvider>
-          <GlobalStyle />
-          <Header />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/survey/:questionNumber">
-              <Survey />
-            </Route>
-            <Route path="/results">
-              <Results />
-            </Route>
-            <Route path="/freelances">
-              <Freelances />
-            </Route>
-            <Route
-              path="/profile/:id"
-              render={(props) => <Profile {...props} />}
-            />
-            <Route path="*">
-              <Error />
-            </Route>
-          </Switch>
-          <Footer />
-        </SurveyProvider>
-      </ThemeProvider>
-    </Router>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <ThemeProvider>
+          <SurveyProvider>
+            <GlobalStyle />
+            <Header />
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/survey/:questionNumber">
+                <Survey />
+              </Route>
+              <Route path="/results">
+                <Results />
+              </Route>
+              <Route path="/freelances">
+                <Freelances />
+              </Route>
+              <Route
+                path="/profile/:id"
+                render={(props) => <Profile {...props} />}
+              />
+              <Route path="*">
+                <Error />
+              </Route>
+            </Switch>
+            <Footer />
+          </SurveyProvider>
+        </ThemeProvider>
+      </Router>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 )
