92983fd7d14cf74808ab069f8a6f8b1610242fd6
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-13 00:42:22 +0200

    Create freelances initialState

---
+++ src/features/freelances.js
@@ -0,0 +1,9 @@
+// Le state initial de la feature freelances
+const initialState = {
+  // le statut permet de suivre l'état de la requête
+  status: 'void',
+  // les données lorsque la requête a fonctionné
+  data: null,
+  // l'erreur lorsque la requête échoue
+  error: null,
+}
