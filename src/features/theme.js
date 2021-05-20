6906a982245992d57dab7df8349c24eef9c153bd
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-20 14:18:41 +0200

    Theme using createReducer

--- src/features/theme.js
+++ src/features/theme.js
@@ -1,15 +1,15 @@
-import { createAction } from '@reduxjs/toolkit'
+import { createAction, createReducer } from '@reduxjs/toolkit'
 
 export const toggleTheme = createAction('theme/toggle')
 
 export const setTheme = createAction('theme/set')
 
-export default function reducer(state = 'light', action) {
-  if (action.type === toggleTheme.toString()) {
-    return state === 'light' ? 'dark' : 'light'
-  }
-  if (action.type === setTheme.toString()) {
-    return action.payload
-  }
-  return state
-}
+export default createReducer('light', (builder) =>
+  builder
+    .addCase(toggleTheme, (state) => {
+      return state === 'light' ? 'dark' : 'light'
+    })
+    .addCase(setTheme, (state, action) => {
+      return action.payload
+    })
+)
