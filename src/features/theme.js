749b8de2a0d5e68ad3526a451017198f41bfa2be
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-20 11:48:35 +0200

    Use createAction for toggleTheme

--- src/features/theme.js
+++ src/features/theme.js
@@ -1,21 +1,16 @@
-// On utilise des variables pour les noms des actions
-// pour éviter les fautes de frappe
-const TOGGLE_THEME = 'theme/toggle'
+import { createAction } from '@reduxjs/toolkit'
+
+export const toggleTheme = createAction('theme/toggle')
+
 const SET_THEME = 'theme/set'
-
-// action creators
-
-export const toggleTheme = () => ({ type: TOGGLE_THEME })
 
 export const setTheme = (theme = 'light') => ({
   type: SET_THEME,
   payload: theme,
 })
 
-// Le reducer
-// on utilise une valeur par défaut pour donner le state initial
 export default function reducer(state = 'light', action) {
-  if (action.type === TOGGLE_THEME) {
+  if (action.type === toggleTheme.toString()) {
     return state === 'light' ? 'dark' : 'light'
   }
   if (action.type === SET_THEME) {
