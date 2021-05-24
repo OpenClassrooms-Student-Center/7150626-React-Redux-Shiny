966f235333acb16acea9060a4e74a75dbb652d39
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-25 00:13:30 +0200

    Use createAction and createReducer for freelances

--- src/features/freelances.js
+++ src/features/freelances.js
@@ -1,4 +1,4 @@
-import produce from 'immer'
+import { createAction, createReducer } from '@reduxjs/toolkit'
 import { selectFreelances } from '../utils/selectors'
 
 // Le state initial de la feature freelances
@@ -10,16 +10,10 @@ const initialState = {
   // l'erreur lorsque la requête échoue
   error: null,
 }
-// Les noms des actions
-const FETCHING = 'freelances/fetching'
-const RESOLVED = 'freelances/resolved'
-const REJECTED = 'freelances/rejected'
-// la requête est en cours
-const freelancesFetching = () => ({ type: FETCHING })
-// la requête a fonctionné
-const freelancesResolved = (data) => ({ type: RESOLVED, payload: data })
-// la requête a échoué
-const freelancesRejected = (error) => ({ type: REJECTED, payload: error })
+
+const freelancesFetching = createAction('freelances/fetching')
+const freelancesResolved = createAction('freelances/resolved')
+const freelancesRejected = createAction('freelances/rejected')
 
 // cette fonction est une action asynchrone
 // elle attend le store redux en paramètre
@@ -46,64 +40,55 @@ export async function fetchOrUpdateFreelances(store) {
   }
 }
 
-export default function freelancesReducer(state = initialState, action) {
-  // on utilise immer pour changer le state
-  return produce(state, (draft) => {
-    // on fait un switch sur le type de l'action
-    switch (action.type) {
-      // si l'action est de type FETCHING
-      case FETCHING: {
-        // si le statut est void
-        if (draft.status === 'void') {
-          // on passe en pending
-          draft.status = 'pending'
-          return
-        }
-        // si le statut est rejected
-        if (draft.status === 'rejected') {
-          // on supprime l'erreur et on passe en pending
-          draft.error = null
-          draft.status = 'pending'
-          return
-        }
-        // si le statut est resolved
-        if (draft.status === 'resolved') {
-          // on passe en updating (requête en cours mais des données sont déjà présentent)
-          draft.status = 'updating'
-          return
-        }
-        // sinon l'action est ignorée
+export default createReducer(initialState, (builder) =>
+  builder
+    // si l'action est de type Fetching
+    .addCase(freelancesFetching, (draft) => {
+      // si le statut est void
+      if (draft.status === 'void') {
+        // on passe en pending
+        draft.status = 'pending'
         return
       }
-      // si l'action est de type RESOLVED
-      case RESOLVED: {
-        // si la requête est en cours
-        if (draft.status === 'pending' || draft.status === 'updating') {
-          // on passe en resolved et on sauvegarde les données
-          draft.data = action.payload
-          draft.status = 'resolved'
-          return
-        }
-        // sinon l'action est ignorée
+      // si le statut est rejected
+      if (draft.status === 'rejected') {
+        // on supprime l'erreur et on passe en pending
+        draft.error = null
+        draft.status = 'pending'
         return
       }
-      // si l'action est de type REJECTED
-      case REJECTED: {
-        // si la requête est en cours
-        if (draft.status === 'pending' || draft.status === 'updating') {
-          // on passe en rejected, on sauvegarde l'erreur et on supprime les données
-          draft.status = 'rejected'
-          draft.error = action.payload
-          draft.data = null
-          return
-        }
-        // sinon l'action est ignorée
+      // si le statut est resolved
+      if (draft.status === 'resolved') {
+        // on passe en updating (requête en cours mais des données sont déjà présentent)
+        draft.status = 'updating'
         return
       }
-      // Sinon (action invalide ou initialisation)
-      default:
-        // on ne fait rien (retourne le state sans modifications)
+      // sinon l'action est ignorée
+      return
+    })
+    // si l'action est de type Resolved
+    .addCase(freelancesResolved, (draft, action) => {
+      // si la requête est en cours
+      if (draft.status === 'pending' || draft.status === 'updating') {
+        // on passe en resolved et on sauvegarde les données
+        draft.data = action.payload
+        draft.status = 'resolved'
         return
-    }
-  })
-}
+      }
+      // sinon l'action est ignorée
+      return
+    })
+    // si l'action est de type Rejected
+    .addCase(freelancesRejected, (draft, action) => {
+      // si la requête est en cours
+      if (draft.status === 'pending' || draft.status === 'updating') {
+        // on passe en rejected, on sauvegarde l'erreur et on supprime les données
+        draft.status = 'rejected'
+        draft.error = action.payload
+        draft.data = null
+        return
+      }
+      // sinon l'action est ignorée
+      return
+    })
+)
