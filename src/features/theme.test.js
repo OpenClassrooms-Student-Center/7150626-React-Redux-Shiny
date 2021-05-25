a9ab5ae791028c9b26f29053e8ace24280bcf77d
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-25 17:06:00 +0200

    Test theme reducer

--- src/features/theme.test.js
+++ src/features/theme.test.js
@@ -1,4 +1,5 @@
 import * as themeActions from './theme'
+import themeReducer from './theme'
 
 describe('Theme actions', () => {
   it('should create a toggle action objet', () => {
@@ -16,5 +17,25 @@ describe('Theme actions', () => {
       type: 'theme/set',
       payload: 'dark',
     })
+  })
+})
+
+describe('Theme reducer', () => {
+  it('should return the initial state when state is undefined', () => {
+    expect(themeReducer(undefined, { type: '@INIT' })).toEqual('light')
+  })
+
+  it('should toggle theme', () => {
+    expect(themeReducer('light', themeActions.toggle())).toEqual('dark')
+    expect(themeReducer('dark', themeActions.toggle())).toEqual('light')
+  })
+
+  it('should set theme', () => {
+    expect(themeReducer('light', themeActions.set('light'))).toEqual('light')
+    expect(themeReducer('dark', themeActions.set('light'))).toEqual('light')
+  })
+
+  it('should return state on invalid action', () => {
+    expect(themeReducer('light', { type: 'INVALID' })).toEqual('light')
   })
 })
