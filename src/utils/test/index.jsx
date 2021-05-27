cf57525950871f337009fd7fb88adb84a5d2a03d
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-27 10:35:09 +0200

    Add Redux Provider in test render

--- src/utils/test/index.jsx
+++ src/utils/test/index.jsx
@@ -1,9 +1,32 @@
 import { render as rtlRender } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
+import themeReducer from '../../features/theme'
+import freelancesReducer from '../../features/freelances'
+import surveyReducer from '../../features/survey'
+import freelanceReducer from '../../features/freelance'
+import resultsReducer from '../../features/results'
+import answersReducer from '../../features/answers'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 
 export function render(ui, options) {
+  const store = configureStore({
+    reducer: {
+      theme: themeReducer,
+      freelances: freelancesReducer,
+      survey: surveyReducer,
+      freelance: freelanceReducer,
+      results: resultsReducer,
+      answers: answersReducer,
+    },
+  })
+
   function Wrapper({ children }) {
-    return <MemoryRouter {...options}>{children}</MemoryRouter>
+    return (
+      <MemoryRouter {...options}>
+        <Provider store={store}>{children}</Provider>
+      </MemoryRouter>
+    )
   }
   rtlRender(ui, { wrapper: Wrapper })
 }
