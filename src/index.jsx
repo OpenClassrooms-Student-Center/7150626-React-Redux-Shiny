2862409a367a8352b10ad369d65f6230a3581894
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-31 16:29:01 +0200

    Setup React-Query

--- src/index.jsx
+++ src/index.jsx
@@ -12,37 +12,45 @@ import Error from './components/Error'
 import GlobalStyle from './utils/style/GlobalStyle'
 import { Provider } from 'react-redux'
 import store from './utils/store'
+// on importe QueryClient et le Provider
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+// on créer le queryClient
+const queryClient = new QueryClient()
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <Router>
-        <GlobalStyle />
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/survey/:questionNumber">
-            <Survey />
-          </Route>
-          <Route path="/results">
-            <Results />
-          </Route>
-          <Route path="/freelances">
-            <Freelances />
-          </Route>
-          <Route
-            path="/profile/:id"
-            render={(props) => <Profile {...props} />}
-          />
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
-        <Footer />
-      </Router>
-    </React.StrictMode>
-  </Provider>,
+  /* On englobe toute l'application dans le Provider */
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <Router>
+          <GlobalStyle />
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/survey/:questionNumber">
+              <Survey />
+            </Route>
+            <Route path="/results">
+              <Results />
+            </Route>
+            <Route path="/freelances">
+              <Freelances />
+            </Route>
+            <Route
+              path="/profile/:id"
+              render={(props) => <Profile {...props} />}
+            />
+            <Route path="*">
+              <Error />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </React.StrictMode>
+    </Provider>
+  </QueryClientProvider>,
   document.getElementById('root')
 )
